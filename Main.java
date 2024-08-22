9103552ab15951ab058576ac11caecdca4ed5978
zawar0045 <zawar0045@gmail.com>
2024-08-22 13:12:26 +0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        String  name=input.nextLine()
+                ;
+        System.out.println("hello! "+name);
+        System.out.println("13");
+       
+    }
+}
